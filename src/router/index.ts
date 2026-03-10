cc56da442b53cb7f1f525bc2e27c295ddbc81c26
Varela <vjosemario38@gmail.com>
2026-03-09 19:25:32 -0600

    Deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
-import AboutPage from '@/views/AboutPage.vue'
+// import AboutPage from '@/views/AboutPage.vue'
 
 const routes = [
   {
@@ -9,11 +9,11 @@ const routes = [
     name: 'Home',
     component: HomePage,
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutPage,
-  }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: AboutPage,
+  // }
 ]
 
 export const router = createRouter({
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = 'MundialScann26K'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
 })
