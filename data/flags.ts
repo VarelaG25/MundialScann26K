c6ffc4ca49497aa86ec32ffda18835812f717cd3
Varela <vjosemario38@gmail.com>
2026-03-09 19:19:00 -0600

    Updates

---
+++ data/flags.ts
@@ -0,0 +1,14 @@
+const flagsLibrary = {
+  "mexico": {
+    name: "México",
+    model: "/models/pyramid.glb",
+    data: { pop: "126M", capital: "CDMX" }
+  },
+  "espana": {
+    name: "España",
+    model: "/models/sagrada_familia.glb",
+    data: { pop: "47M", capital: "Madrid" }
+  }
+}
+
+export default flagsLibrary;
